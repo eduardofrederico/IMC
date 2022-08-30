cf0d7bf5312f7c390c415d2b3bbec6342c26b02e
eduardofrederico <eduardo.frederico@icloud.com>
2022-08-29 22:26:53 -0300

    Initial Commit

---
+++ IMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Eduardo Frederico on 29/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
